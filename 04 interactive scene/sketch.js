f61839b6489d550e03607a3bc0c69009bd246cbf
BaconatorDater <7011374@spsd.sk.ca>
2026-02-12 11:16:34 -0600

    Update sketch.js

--- 04 interactive scene/sketch.js
+++ 04 interactive scene/sketch.js
@@ -15,10 +15,21 @@ function scene(){
   circle(110,110,90)
   fill(0,100,0)
   rect(0, 400, 500, 200)
+  triangle(130,375,230,375,180,250)
+  fill(133, 124, 99)
+  rect(170,400,25,-25)
+}
+
+function displayMouse(){
+  textSize(40);
+  textAlign(LEFT, TOP)
+  fill(0,0,0);
+  text(mouseX + "," + mouseY,    mouseX, mouseY);
 }
 
 
 function draw() {
   background(220);
   scene()
+  displayMouse()
 }
