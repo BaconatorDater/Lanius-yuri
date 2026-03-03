78a21077785e829f159d67a94e5ce5f54b15e73b
BaconatorDater <7011374@spsd.sk.ca>
2026-03-03 12:22:14 -0600

    Update sketch.js

--- Perlin noise/sketch.js
+++ Perlin noise/sketch.js
@@ -5,14 +5,30 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
+let rectWidth = 1
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(500, 500);
 }
 
 function generateTerrain(){
-  
+  strokeWeight(rectWidth)
+  //using a loop to make a number of rectangles with random height.
+  for(let x = 0; x < width; x+=rectWidth){
+    //generate random negative height.
+    //replace this with noise()
+    let rectHeight = random(0, height*0.75);
+
+
+    rect(x,height,rectWidth,-rectHeight);
+  }
 }
+
+fiunction 
 function draw() {
+  //locks in this as a starting point (seed 25 from random, the next one will be seed 26, etc.)
+  randomSeed(25);
+  //stabilize once per frame.
   background(220);
+  generateTerrain()
+
 }
