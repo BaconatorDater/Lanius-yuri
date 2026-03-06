173d6f65426fe50ddd5edd27f45cab78e1ce4ca0
BaconatorDater <7011374@spsd.sk.ca>
2026-03-06 11:38:57 -0600

    Update sketch.js

--- 11Nested loops and popping bubbles/sketch.js
+++ 11Nested loops and popping bubbles/sketch.js
@@ -6,10 +6,32 @@
 // - describe what you did to take this project "above and beyond"
 
 
+let bubbleSize = 30;
+let bubbles = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
+}
+
+function drawbubble(){
+  for(let b of bubbles){
+    circle(b.x, b.y,bubbleSize);
+  }
+}
+
+function generatePoints(){
+  //simple nested loop test to make ordered pairs:
+  //x: 0, 30, 60      y:0, 30, 60
+  for(let x = 0; x <= 60; x+=30){
+    //x: 0, 30, 60
+    for(let y = 0; y <= 60; y+=30){
+      // y:0, 30, 60
+      print(x,y);
+      circle(x, y, 30);
+    }
+  }
 }
