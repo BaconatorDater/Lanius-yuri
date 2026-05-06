d7acfe34c5cbdb07665a964db1eea918d86042df
BaconatorDater <7011374@spsd.sk.ca>
2026-05-06 11:54:38 -0600

    more fixing

---
+++ 11 CAPSTONE PROJECT!!!!!!!/STSFTL/CardData/AtkBaisic.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@export var CardName: String = "Attack"
+@export var CardDescription: String = "Deal 10 Damage to the enemy, base cost: 1 energy"
+@export var CardCost: int = 1
+@export var CardImage: Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
